f0bcf8fb7b51b6cfdf2b82e9e584b06330452525
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-19 15:54:42 +0100

    added fight images

--- SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
+++ SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
@@ -15,6 +15,14 @@ Iterate(tilemap, lambda x: x.Draw(screen))
 centerImage = pygame.image.load("Images\center.png")
 screen.blit(pygame.transform.scale(centerImage, (10*TILESIZE,10*TILESIZE)),(OFFSET + (2*TILESIZE),2*TILESIZE))
 
+#add fight images
+fightImage = pygame.transform.scale(pygame.image.load("Images\Fight.png"), (2*TILESIZE,1*TILESIZE))
+screen.blit(fightImage, (OFFSET + (6*TILESIZE),1*(TILESIZE*0.5))) #top
+#screen.blit(pygame.transform.rotate(fightImage,180), (OFFSET + (6*TILESIZE),1*(TILESIZE*0.5))) #top
+screen.blit(fightImage, (OFFSET + (6*TILESIZE),12*TILESIZE + 1*(TILESIZE*0.5))) #bottom
+screen.blit(pygame.transform.rotate(fightImage,90), (OFFSET + (12*TILESIZE) + 1*(TILESIZE*0.5),6*TILESIZE)) #left
+screen.blit(pygame.transform.rotate(fightImage,270), (OFFSET + 1*(TILESIZE*0.5),6*TILESIZE)) #right
+
 pygame.display.update()
 
 time.sleep(5)
