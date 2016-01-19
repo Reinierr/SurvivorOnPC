88c24f68c8e1dc85154d471d4c511adb34f4aced
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-19 15:10:21 +0100

    fixed center image

--- SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
+++ SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
@@ -9,8 +9,11 @@ pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
 tilemap = Empty()
-tilemap = Tile.Board(screen)
+tilemap = Tile.Board()
 Iterate(tilemap, lambda x: x.Draw(screen))
+
+centerImage = pygame.image.load("Images\center.png")
+screen.blit(pygame.transform.scale(centerImage, (10*TILESIZE,10*TILESIZE)),(OFFSET + (2*TILESIZE),2*TILESIZE))
 
 pygame.display.update()
 
