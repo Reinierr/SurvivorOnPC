0852fccf8de4108a71c8f6f5afad6cc1484dab17
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-19 15:41:02 +0100

    added comments

--- SurvivorOnPC/SurvivorOnPC/Constants.py
+++ SurvivorOnPC/SurvivorOnPC/Constants.py
@@ -1,7 +1,7 @@
-﻿
-#constants representing colors
+﻿#screen size
 SIZE = width, height = 1280, 720
 
+#constants representing colors
 BLUE = (71,106,165)
 RED = (190,61,52)
 GREEN = (119,182,60)
@@ -10,7 +10,7 @@ GREY = (200,200,200)
 WHITE = (255,255,254)
 BLACK = (0,0,0)
 
+#boardsize and tilesize
 TILESIZE = int (SIZE[1] / 14)
 MAPSIZE = 14
 OFFSET = (SIZE[0] - (TILESIZE*MAPSIZE)) / 2
-#patch
