1c8898b644bc45f7568e3648006acbfda4e2dd34
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-19 14:25:04 +0100

    grid in node

--- SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
+++ SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
@@ -8,43 +8,39 @@ from Constants import *
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
-
-
-tilemap = [
-            [BLUE,BLUE,BLUE,GREY,WHITE,GREY,WHITE,WHITE,GREY,WHITE,GREY,RED,RED,RED],
-            [BLUE,BLUE,BLUE,GREY,WHITE,GREY,WHITE,WHITE,GREY,WHITE,GREY,RED,RED,RED],
-            [BLUE,BLUE,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,RED,RED],
-            [GREY,GREY,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,GREY,GREY],
-            [WHITE,WHITE,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,WHITE,WHITE],
-            [GREY,GREY,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,GREY,GREY],
-            [WHITE,WHITE,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,WHITE,WHITE],
-            [WHITE,WHITE,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,WHITE,WHITE],
-            [GREY,GREY,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,GREY,GREY],
-            [WHITE,WHITE,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,WHITE,WHITE],
-            [GREY,GREY,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,GREY,GREY],
-            [YELLOW,YELLOW,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,GREEN,GREEN],
-            [YELLOW,YELLOW,YELLOW,GREY,WHITE,GREY,WHITE,WHITE,GREY,WHITE,GREY,GREEN,GREEN,GREEN],
-            [YELLOW,YELLOW,YELLOW,GREY,WHITE,GREY,WHITE,WHITE,GREY,WHITE,GREY,GREEN,GREEN,GREEN]
-          ]
+def Color(column,row):
+  x = OFFSET + (column * TILESIZE)
+  y = row * TILESIZE
+  grey = [3,5,8,10]
+  white = [4,6,7,9]
+  if(x >= OFFSET and x < OFFSET + TILESIZE *3 and y < TILESIZE *3):
+    return BLUE
+  elif(x >= OFFSET + TILESIZE * 11 and x <= OFFSET + TILESIZE*14 and y < TILESIZE*3):
+    return RED
+  elif(x >= OFFSET and x < OFFSET + TILESIZE *3 and y >= TILESIZE*11):
+    return YELLOW
+  elif(x >= OFFSET + TILESIZE * 11 and x <= OFFSET + TILESIZE*14 and y >= TILESIZE*11):
+    return GREEN
+  elif(column in grey or row in grey):
+    return GREY
+  elif(column in white or row in white):
+    return WHITE
+  else:
+    return BLACK
 
 tilemap = Empty()
 for row in range(MAPSIZE):
   for column in range(MAPSIZE):
-    tilemap = Node(Tile(Vector2(OFFSET,0),WHITE),tilemap)
+    tilemap = Node(Tile(Vector2(OFFSET+(column*TILESIZE),row*TILESIZE),Color(column,row)),tilemap)
 
 Iterate(tilemap, lambda x: x.Draw(screen))
 
-#for row in range(MAPSIZE):
-#  for column in range(MAPSIZE):
-#    pygame.draw.rect(screen, tilemap[row][column], (column*TILESIZE+OFFSET,row*TILESIZE,TILESIZE,TILESIZE))
-
 centerImage = pygame.image.load("Images\center.png")
-#screen.blit(pygame.transform.scale(centerImage, (10*TILESIZE,10*TILESIZE)),(OFFSET + (2*TILESIZE),2*TILESIZE))
+screen.blit(pygame.transform.scale(centerImage, (10*TILESIZE,10*TILESIZE)),(OFFSET + (2*TILESIZE),2*TILESIZE))
 
 pygame.display.update()
 
 time.sleep(5)
-
 
 def Main():
   #game loop
