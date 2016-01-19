891564276cc53e91a747d5ad08a47ac2e0d0a90e
Reinierr <reinier.dorst1@hotmail.com>
2016-01-19 12:46:53 +0100

    gamemenu met buttons

--- SurvivorOnPC/SurvivorOnPC/Node.py
+++ SurvivorOnPC/SurvivorOnPC/Node.py
@@ -1,8 +1,8 @@
-class Empty():
+﻿class Empty():
   def __init__(self):
     self.IsEmpty = True
 
 class Node():
   def __init__(self, value, tail):
-    self.IsEmpty = 
+    self.IsEmpty = Empty
     self.Value = value
