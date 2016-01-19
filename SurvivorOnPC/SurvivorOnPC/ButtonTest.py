779f27ae83de48aae0a673834e8fdc52ce1a72e0
blackhit55 <marlowlieshout@hotmail.com>
2016-01-19 13:21:47 +0100

    test

--- SurvivorOnPC/SurvivorOnPC/ButtonTest.py
+++ SurvivorOnPC/SurvivorOnPC/ButtonTest.py
@@ -1,87 +1,63 @@
-﻿import pygame
-import sys
-
-pygame.init()
-
-scr_width = 400
-scr_height = 400 
-class HelpMenu():
-    def __init__(self, screen):
-        self.screen = screen
-        self.scr_width = screen.get_rect().width
-        self.scr_height = screen.get_rect().height
-        self.bg_color = (255,0,0)
-
-        self.font = pygame.font.SysFont(None, 30)
-        self.font_color = (255, 255, 255)
-
-        labelb = self.font.render('Back', 1, self.font_color)
-        self.back = [labelb, self.scr_width / 2 - labelb.get_rect().width / 2, self.scr_height / 2 + 100]
-        
-        self.information = 'Tekst'
-        labelinfo = self.font.render(self.information, 1, self.font_color)
-
-        self.screen.fill(self.bg_color)
-
-        bb = self.screen.blit(self.back[0], (self.back[1],self.back[2]))
-        self.screen.blit(labelinfo,(0,0))
-        print('enter')
-        #if pygame.event.type == pygame.MOUSEBUTTONDOWN:
-           # pos = pygame.mouse.get_pos()
-            #if bb.collidepoint(pos):
-             #   print('test')
-
-class GameMenu():
-    def __init__(self, screen):
-        self.screen = screen
-        self.scr_width = screen.get_rect().width
-        self.scr_height = screen.get_rect().height
-        self.bg_color = (0,0,0)
-
-        self.font = pygame.font.SysFont(None, 30)
-        self.font_color = (255, 255, 255)
-           
-        #buttons
-        labels = self.font.render('Start', 1, self.font_color)
-        labelc = self.font.render('Exit', 1, self.font_color)
-        labelh = self.font.render('How to play', 1, self.font_color)
-        self.start = [labels, self.scr_width / 2 - labels.get_rect().width / 2, self.scr_height / 2 - 60]
-        self.close = [labelc, self.scr_width / 2 - labelc.get_rect().width / 2, self.scr_height / 2]
-        self.help = [labelh, self.scr_width / 2 - labelh.get_rect().width / 2, self.scr_height / 2 - 30]
+﻿#!/usr/bin/python
  
-
+import pygame
+ 
+pygame.init()
+ 
+ 
+class GameMenu():
+    def __init__(self, screen, items, bg_color=(0,0,0), font=None, font_size=30,
+                    font_color=(255, 255, 255)):
+        self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
+ 
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+ 
+        self.items = items
+        self.font = pygame.font.SysFont(font, font_size)
+        self.font_color = font_color
+ 
+        self.items = []
+        for index, item in enumerate(items):
+            label = self.font.render(item, 1, font_color)
+ 
+            width = label.get_rect().width
+            height = label.get_rect().height
+ 
+            posx = (self.scr_width / 2) - (width / 2)
+            # t_h: total height of text block
+            t_h = len(items) * height
+            posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
+ 
+            self.items.append([item, label, (width, height), (posx, posy)])
+ 
     def run(self):
         mainloop = True
         while mainloop:
-
+            # Limit frame speed to 50 FPS
+            self.clock.tick(50)
+ 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     mainloop = False
  
+            # Redraw the background
             self.screen.fill(self.bg_color)
-
-            bs = self.screen.blit(self.start[0], (self.start[1],self.start[2]))
-            bh = self.screen.blit(self.help[0], (self.help[1],self.help[2]))
-            bc = self.screen.blit(self.close[0], (self.close[1],self.close[2]))
-
+ 
+            for name, label, (width, height), (posx, posy) in self.items:
+                self.screen.blit(label, (posx, posy))
+ 
             pygame.display.flip()
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                if bs.collidepoint(pos):
-                    #run other program
-                    print('start game')
-                elif bh.collidepoint(pos):
-                    pygame.display.set_caption('How to play Menu')
-                    hm = HelpMenu(screen)
-                    print(''
-                elif bc.collidepoint(pos):
-                    mainloop = False
-                    sys.exit()
-            
-
-screen = pygame.display.set_mode((scr_width, scr_height))
-
-pygame.display.set_caption('Game Menu')
-gm = GameMenu(screen)
-gm.run()
+ 
+ 
+if __name__ == "__main__":
+    # Creating the screen
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+ 
+    menu_items = ('Start', 'Quit')
+ 
+    pygame.display.set_caption('Game Menu')
+    gm = GameMenu(screen, menu_items)
+    gm.run()
--- SurvivorOnPC/SurvivorOnPC/Interaction.py
+++ SurvivorOnPC/SurvivorOnPC/Interaction.py
@@ -1,4 +1,4 @@
-import pygame
+﻿import pygame
 import sys
 
 pygame.init()
