9a4645c5fb682acf47e17703b22308750402248c
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-19 10:18:52 +0100

    added game loop

--- SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
+++ SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
@@ -1,2 +1,12 @@
 ﻿import pygame
 from Node import *
+
+pygame.init()
+size = width, height = 1280, 720
+screen = pygame.display.set_mode(size)
+
+def Main():
+  #game loop
+  print("<3")
+
+Main()  
