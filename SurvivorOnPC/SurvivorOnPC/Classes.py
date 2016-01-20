5dd590a624c5899cfaf39d241b10486e650e02b6
blackhit55 <marlowlieshout@hotmail.com>
2016-01-20 10:27:39 +0100

    added classes file

---
+++ SurvivorOnPC/SurvivorOnPC/Classes.py
@@ -0,0 +1,10 @@
+class ScoreCard:
+    def __init__(self,name,color,dice_score,damage,condition):
+        self.name = name
+        self.color = color
+        self.dice_score = dice_score
+        self.damage = damage
+        self.condition = condition
+
+
+
