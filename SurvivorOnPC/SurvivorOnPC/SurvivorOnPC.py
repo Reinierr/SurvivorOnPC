31084a39fb43c3ac6aff9d4d833ab4ce9441d9d0
Reinierr <reinier.dorst1@hotmail.com>
2016-01-21 11:46:05 +0100

    main class name edited

--- SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
+++ SurvivorOnPC/SurvivorOnPC/SurvivorOnPC.py
@@ -19,7 +19,7 @@ def GameBoard(screen):
   pygame.display.set_caption('SurvivorOnPC')
   pygame.display.update()
 
-class GameMenu():
+class Game():
     def __init__(self, screen):
         self.screen = screen
         self.scr_width = screen.get_rect().width
@@ -102,5 +102,5 @@ class GameMenu():
 screen = pygame.display.set_mode(SIZE)
 
 pygame.display.set_caption('Game Menu')
-gm = GameMenu(screen)
+gm = Game(screen)
 gm.run()
