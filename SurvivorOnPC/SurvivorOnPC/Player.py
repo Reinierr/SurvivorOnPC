32198e0a9abb6efc751f37a1681732a74bca3b26
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-22 10:45:44 +0100

    Assign lifepoints and conditionpoints to each player on create

--- SurvivorOnPC/SurvivorOnPC/Player.py
+++ SurvivorOnPC/SurvivorOnPC/Player.py
@@ -6,6 +6,8 @@ class Player:
   def __init__(self,position,texture):
     self.Tile = position
     self.Texture = texture
+    self.Life = 100
+    self.Condition = 15
 
   def Move(self,tilemap,steps):
     newIndex = self.Tile.Index + steps
