703e0715f7a152e513dbf4a29cca295719315b95
Reinierr <reinier.dorst1@hotmail.com>
2016-01-22 13:27:25 +0100

    further improvements to playerselect, color choice

---
+++ SurvivorOnPC/SurvivorOnPC/Display.py
@@ -0,0 +1,10 @@
+import pygame
+from Constants import *
+from Button import *
+
+class Display():
+    def __init__(self, screen):
+        self.screen = screen
+
+    def PickColorButtons(self):
+        return True
