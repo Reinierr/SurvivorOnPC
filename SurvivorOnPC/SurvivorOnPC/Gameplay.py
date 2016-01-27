f5036d7a9b8fc38c5cf101a9c3d75bafde6abdfe
Marcel Stassen <Marcel_Stassen@hotmail.com>
2016-01-27 12:34:35 +0100

    epic update playerlist

--- SurvivorOnPC/SurvivorOnPC/Gameplay.py
+++ SurvivorOnPC/SurvivorOnPC/Gameplay.py
@@ -12,11 +12,13 @@ def PlayerList(colornumber):
   tilemap = CreateMap()
   playeramount = len(colornumber)
   while playeramount != 0:
-      if playeramount-1 == 0:
+      if playeramount == len(colornumber):
          players = Node(Player( tilemap, colornumber[playeramount-1]),players)
          players.Value.Turn = True
+         players.Value.Number = playeramount
       else:
          players = Node(Player( tilemap, colornumber[playeramount-1]),players)
+         players.Value.Number = playeramount 
       players
       playeramount -= 1
   return players
@@ -83,25 +85,25 @@ def playerturn(screen, players, dicenumber):
                 #-Turn player ends-#
                 if not players.Tail.IsEmpty:
                     players.Tail.Value.Turn = True
-                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(cnt), 1, colors[int(math.floor(players.Tail.Value.Home)/10)])
+                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(players.Tail.Value.Number), 1, colors[int(math.floor(players.Tail.Value.Home)/10)])
                     screen.blit(playerlabel,(0, SIZE[1] - 25))
                     return players
                 else:
                     newplayer.Value.Turn = True
-                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(cnt), 1, colors[int(math.floor(newplayer.Value.Home)/10)])
+                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(newplayer.Value.Number), 1, colors[int(math.floor(newplayer.Value.Home)/10)])
                     screen.blit(playerlabel,(0, SIZE[1] - 25))
                     return newplayer
         else:
                 if not players.Tail.IsEmpty:
                     players.Tail.Value.Turn = True
-                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(cnt), 1, colors[int(math.floor(players.Tail.Value.Home)/10)])
+                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(players.Tail.Value.Number), 1, colors[int(math.floor(players.Tail.Value.Home)/10)])
                     screen.blit(playerlabel,(SIZE[0]/2-playerlabel.get_rect().width/2, SIZE[1] - 25))
                     return players
                 else:
                     newplayer.Value.Turn = True
-                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(cnt), 1, colors[int(math.floor(newplayer.Value.Home)/10)])
+                    playerlabel = FONT_TEXT.render('Current turn: Player '+str(newplayer.Value.Number), 1, colors[int(math.floor(newplayer.Value.Home)/10)])
                     screen.blit(playerlabel,(SIZE[0]/2-playerlabel.get_rect().width/2, SIZE[1] - 25))
                     return newplayer
-            
+           
         players = players.Tail
  
