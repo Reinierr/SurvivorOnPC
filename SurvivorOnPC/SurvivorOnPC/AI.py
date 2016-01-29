32d157c8bcd419923ca297b2424860ce62dda351
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-29 12:55:21 +0100

    Created Hard/Easy AI

--- SurvivorOnPC/SurvivorOnPC/AI.py
+++ SurvivorOnPC/SurvivorOnPC/AI.py
@@ -3,13 +3,67 @@ from Cards import *
 from Node import *
 import math
 
-def AI(player, dice):
+#[Schade, Conditie, Dice, Choice]
+
+def HardAI(player, dice):
   home = player.Home
   index = player.Tile.Index
   condition = player.Condition
   
+  #calculate distance to home tile
   if index > home:
     distance = home + 40 - index
   elif index <= home:
     distance = home - index
+  
+  #create list with possible actions
+  scorecard = Empty()
+  scorecard = scorecards[int(math.floor(home/10))]
+  possibleActions = Empty()
+  possibleActions = scorecard.Filter(lambda x: x[2] == dice).Filter(lambda x: not (x[1] > condition))
 
+  #return 0 dmg if no possible actions
+  if possibleActions.IsEmpty:
+    return [0, 0, home]
+
+  #create list with dmg/cond ratio
+  x = possibleActions
+  dmgCost = [] #(dmg / cond)
+  while not x.IsEmpty:
+    #[dmgCost, dmg, cond]
+    dmgCost.append([(x.Value[0]/x.Value[1]), x.Value[0], x.Value[1]])
+    x = x.Tail
+
+  if distance > 20:
+    dmgCost.sort(0, True)
+  else:
+    dmgCost.sort(1, True)
+
+  choice = dmgCost[0]
+
+  return [choice[1],choice[2], home]
+
+def EasyAI(player,dice):
+  home = player.Home
+  condition = player.Condition
+
+  #create list with possible actions
+  scorecard = Empty()
+  scorecard = scorecards[int(math.floor(home/10))]
+  possibleActions = Empty()
+  possibleActions = scorecard.Filter(lambda x: x[2] == dice).Filter(lambda x: not (x[1] > condition))
+
+  #return 0 dmg if no possible actions
+  if possibleActions.IsEmpty:
+    return [0, 0, home]
+
+  #create list with dmg/cond ratio
+  x = possibleActions
+  actions = []
+  while not x.IsEmpty:
+    actions.append(x.Value)
+    x = x.Tail
+
+  choice = actions[random.randint(0,len(actions)-1)]
+
+  return [choice[0], choice[1], home]
