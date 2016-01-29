c0095f3c5e72d9499a98521bfb4b93119a6e7131
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-01-29 11:08:48 +0100

    started on AI. calculated distance to hometile

--- SurvivorOnPC/SurvivorOnPC/AI.py
+++ SurvivorOnPC/SurvivorOnPC/AI.py
@@ -1,3 +1,15 @@
-import random
+﻿import random
 from Cards import *
 from Node import *
+import math
+
+def AI(player, dice):
+  home = player.Home
+  index = player.Tile.Index
+  condition = player.Condition
+  
+  if index > home:
+    distance = home + 40 - index
+  elif index <= home:
+    distance = home - index
+
